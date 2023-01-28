9fabe0d4b987aa6da37675af23085f2df1aa08b3
Sergey <diuk@inbox.ru>
2023-01-28 17:46:42 +0300

    коммит 1

---
+++ task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"число A степени B равно: {Math.Pow(A, B)}");
---
+++ task_29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int N = new Random().Next(3, 20);
+int[] array = new int[N];
+FillArray(array);
+void FillArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 9);
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("\b\b ");
+    Console.WriteLine("]");
+}
