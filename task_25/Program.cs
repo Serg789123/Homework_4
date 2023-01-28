1f5cc1f0039cd21548a7bd86823f66721eabe89d
Serg789123 <121310687+Serg789123@users.noreply.github.com>
2023-01-28 20:23:06 +0300

    Update Program.cs

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -9,3 +9,16 @@ int A = int.Parse(Console.ReadLine());
 Console.Write("Введите число B: ");
 int B = int.Parse(Console.ReadLine());
 Console.WriteLine($"число A степени B равно: {Math.Pow(A, B)}");
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int n = 1;
+int result = A;
+while (n < B)
+{
+    result = result * A;
+    n++;
+}
+Console.WriteLine($"число A степени B равно: {result}");
